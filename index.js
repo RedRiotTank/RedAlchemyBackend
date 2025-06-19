737d18d7777c5bb644019a1e0c591ad4ce4514a6
AlbertoPlaza <albertoplazamontesdm@gmail.com>
2025-06-19 13:16:45 +0200

    🚧 WIP: cors st pol

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
 const allowedOrigins = [
   "http://localhost:4321",
   "https://redalchemy.redriottank.com",
@@ -34,20 +33,14 @@ app.use(
 
 app.options("*", cors());
 
-app.use((req, res, next) => {
-  console.log("Request received:", req.method, req.url);
-  next();
-});
-
 app.use(express.json());
 
 app.use((req, res, next) => {
   console.log("Request received:", req.method, req.url);
-  res.setHeader("Access-Control-Allow-Origin", "*");
   next();
 });
 
-// Routes
+// Rutas
 app.get("/elements", getAllElements);
 app.get("/elements/base", getBaseElements);
 app.get("/fusion", findFusion);
