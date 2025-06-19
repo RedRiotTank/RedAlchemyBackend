761d7ea0ae7d2257a9e7a0addaa2f71b80f4a5d3
AlbertoPlaza <albertoplazamontesdm@gmail.com>
2025-06-19 13:03:50 +0200

    💩 test: middleware cors

--- index.js
+++ index.js
@@ -22,6 +22,12 @@ app.use(
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log("Request received:", req.method, req.url);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  next();
+});
+
 // Routes
 app.get("/elements", getAllElements);
 app.get("/elements/base", getBaseElements);
